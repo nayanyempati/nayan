47b5776792dbcee82f08f27af9dc5b7cfc210142
nayanyempati <nayanyempati@gmail.com>
2022-11-25 12:39:28 +0000

    done

--- nayan.py
+++ nayan.py
@@ -1,3 +1,5 @@
+#https://github.com/nayanyempati/nayan/commits/main/nayan.py
+
 # Creating employee class
 class employee:
     def __init__(self, name, weeklyhours, rate,overtimeRate,weeklytaxcredit):
@@ -8,11 +10,20 @@ class employee:
         self.weeklytaxcredit=weeklytaxcredit
 
     def computeWeeklyPay(self, hours):
+        if(self.overtimeRate<0):
+            return "Overtime cannot be negative"
         compute = (hours*self.rate)+(4*self.overtimeRate)
-        return compute
+        if(compute <0):
+            #Returing 0 if weekly pay is negative
+            return 0
+        else:  
+            return compute
 
     def computeTax(self, grossPay):
         tax=(0.4*grossPay)-self.weeklytaxcredit
+        if(tax<0):
+            # Returing 0 if tax is negative
+            return 0
         return tax
     
 data=employee('nayan',35,11,15,70)
@@ -21,4 +32,11 @@ computeWeeklyPay=data.computeWeeklyPay(35)
 computeTax = data.computeTax(computeWeeklyPay)
 
 print("Weekly Pay: "+str(computeWeeklyPay)+" and compute tax: "+ str(computeTax))
-    
+
+
+
+
+
+
+        
+
