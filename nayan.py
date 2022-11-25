7b3313f19c7248f5a6a97865ff5affda2424650e
nayanyempati <nayanyempati@gmail.com>
2022-11-25 11:34:00 +0000

    AddedaCreated Class and attributes

--- nayan.py
+++ nayan.py
@@ -1,1 +1,8 @@
-print ('i\'m nayan')
+# Creating employee class
+class employee:
+    def _init_(self, name, weeklyhours, rate,overtimeRate,weeklytaxcredit):
+        self.name=name
+        self.weeklyhours=weeklyhours
+        self.rate=rate
+        self.overtimeRate=overtimeRate
+        self.weeklytaxcredit=weeklytaxcredit
